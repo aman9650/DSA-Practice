efd76169215f6a6263880b0b9399589a1fb3194a
Aman Gautam <97427361+aman9650@users.noreply.github.com>
2023-01-07 23:19:45 +0530

    Create Main.java

---
+++ Stack/Balanced_paranthesis/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main
+{
+    static boolean matching(char a,char b){
+        return((a=='(') && b==')') || (a=='{' && a=='}') || (a=='[' && a==']');
+    }
+    static boolean isBalanced(String str){
+        Deque<Character> s= new ArrayDeque<>();
+        
+        for(int i=0;i<str.length();i++){
+            char x=str.charAt(i);
+            if(x=='(' || x=='{' || x=='['){
+                s.push(x);
+            }
+            else{
+                if(s.isEmpty()==true){
+                    return false;
+                }
+                else if(matching(s.peek(),x)==false){
+                    return false;
+                }
+                else {
+                    s.pop();
+                }
+            }
+        }
+        return (s.isEmpty()==true);
+    }
+	public static void main(String[] args) {
+	    String expr="(())";
+		if (isBalanced(expr))
+            System.out.println("Balanced ");
+        else
+            System.out.println("Not Balanced ");
+    }
+	}
+
